719d8995aa835263713f92e7752d6988bcd92926
doyoung park <asdlkj0104@gmail.com>
2025-01-21 15:12:29 +0900

    Update 250121

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,46 +1,37 @@
-import BackButton from './BackButton';
+"use client";
+
 import style from './signup.module.css';
-import {redirect} from "next/navigation";
+import onSubmit from '../_lib/signup';
+import BackButton from "@/app/(beforeLogin)/_component/BackButton";
+import { useFormStatus, useFormState } from 'react-dom';
+
+function showMessage(message: string | null | undefined) {
+  if (message === 'no_id') {
+    return '아이디를 입력하세요.';
+  }
+  if (message === 'no_name') {
+    return '닉네임을 입력하세요.';
+  }
+  if (message === 'no_password') {
+    return '비밀번호를 입력하세요.';
+  }
+  if (message === 'no_image') {
+    return '이미지를 업로드하세요.';
+  }
+  if (message === 'user_exists') {
+    return '이미 사용 중인 아이디입니다.';
+  }
+  if (message === 'nickname must be a string') {
+    return '닉네임이 필요합니다.';
+  }
+  return message;
+}
 
 export default function SignupModal() {
-  
-  const submit = async (formData: FormData) => {
-    "use server"
-    if (!formData.get('id')) {
-      throw new Error('no_id');
-    }
-    if (!formData.get('name')) {
-      throw new Error('no_name');
-    }
-    if (!formData.get('password')) {
-      throw new Error('no_password');
-    }
-    if (!formData.get('image')) {
-      throw new Error('no_image');
-    }
+  const [state, formAction] = useFormState(onSubmit, { message: null });
+  const { pending } = useFormStatus();
 
-    let shouldRedirect = false;
-    try {
-      const response: Response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users`, {
-        method: 'post',
-        body: formData,
-        credentials: 'include'
-      });
-      console.log(response.status);
-      if(response.status === 403){
-        throw new Error('user_exists');
-      }
-      console.log(await response.json());
-
-      shouldRedirect = true;
-    } catch (error) {
-      console.error(error);
-    }
-
-    if(shouldRedirect){
-      redirect('/home');
-    }
-  }
+  console.log('state', state);
 
   return (
     <>
@@ -50,27 +41,37 @@ export default function SignupModal() {
             <BackButton />
             <div>계정을 생성하세요.</div>
           </div>
-          <form action={submit}>
+          <form action={formAction}>
             <div className={style.modalBody}>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="id">아이디</label>
-                <input id="id" className={style.input} type="text" placeholder="" required />
+                <input id="id" name="id" className={style.input} type="text" placeholder=""
+                       required defaultValue={state.id as string}
+                />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="name">닉네임</label>
-                <input id="name" className={style.input} type="text" placeholder="" required />
+                <input id="name" name="name" className={style.input} type="text" placeholder=""
+                       required defaultValue={state.nickname as string}
+                />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="password">비밀번호</label>
-                <input id="password" className={style.input} type="password" placeholder="" required />
+                <input id="password" name="password" className={style.input} type="password" placeholder=""
+                       required defaultValue={state.password as string}
+                />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="image">프로필</label>
-                <input id="image" className={style.input} type="file" accept="image/*" required />
+                <input id="image" name="image"
+                       required className={style.input} type="file" accept="image/*"
+                       defaultValue={state.image as string}
+                />
               </div>
             </div>
             <div className={style.modalFooter}>
-              <button type="submit" className={style.actionButton}>가입하기</button>
+              <button type="submit" className={style.actionButton} disabled={pending}>가입하기</button>
+              <div className={style.error}>{showMessage(state?.message)}</div>
             </div>
           </form>
         </div>
