c6eac979f44c4c8ab3278c36981a2f7c12a35825
doyoung park <asdlkj0104@gmail.com>
2025-01-24 14:44:40 +0900

    Update 250124

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { MSWProvider } from './_components/MSWComponent'
+import AuthSession from "@/app/_components/AuthSession";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <MSWProvider>
+          <AuthSession>
           {children}
+          </AuthSession>
         </MSWProvider>
       </body>
     </html>
